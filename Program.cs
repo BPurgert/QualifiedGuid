aa8b3a8b376d9594d61b82d54632e0ab8a4f8c13
Brian Purgert <bpurgert@gmail.com>
2023-11-06 07:16:21 -0500

    Adding files

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+namespace Quid
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string q1 = GenerateQuidString("75");
+            Console.WriteLine(q1);
+            DecodeQuid(q1);
+
+            Console.WriteLine();
+
+            q1 = GenerateQuidString("9");
+            Console.WriteLine(q1);
+            DecodeQuid(q1);
+
+            Console.WriteLine();
+
+            q1 = GenerateQuidString("999");
+            Console.WriteLine(q1);
+            DecodeQuid(q1);
+        }
+
+        private static string GenerateQuidString(string userId)
+        {
+            userId = userId.PadLeft(2, '0');
+            string hexEpoch = DateTimeOffset.UtcNow.ToUnixTimeSeconds().ToString("X2");
+            string random = Guid.NewGuid().ToString().Split('-')[4];
+
+            string quid = $"555{hexEpoch.Substring(0, 5)}-{hexEpoch.Substring(5, 3)}{userId.Substring(0,1)}-1{userId.Substring(1,1)}00-c000-{random}";
+            return quid;
+        }
+
+        private static void DecodeQuid(string quid)
+        {
+            bool isGuid = Guid.TryParse(quid, out Guid g);
+            string unformatted = quid.Replace("-", "").Trim();
+            string prefix = unformatted.Substring(0, 3);
+            string unixEpochHex = unformatted.Substring(3, 8);
+            string userId = $"{unformatted.Substring(11, 1)}{unformatted.Substring(13, 1)}";
+
+            long epoch = Convert.ToInt64(unixEpochHex, 16);
+            DateTimeOffset epochDateTimeOffset = DateTimeOffset.FromUnixTimeSeconds(epoch);
+
+            Console.WriteLine($"Is GUID: {isGuid}");
+            Console.WriteLine($"User ID: {userId}");
+            Console.WriteLine($"Unix Epoch hex: {unixEpochHex}");
+            Console.WriteLine($"Converted to datetime: {epochDateTimeOffset}");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,36 @@
+﻿The Qualified GUID (QUID)
+
+The Qualified GUID (QUID) is designed to be used in testing situations where you want to be 
+able to identity test data. The QUID always starts with 555, inspired by Hollywood phone numbers.
+The QUID stores the time it was generated and, optionally, an ID for the user or system that 
+generated the QUID. There are 12 bits available for user / application specific data. 8 bits 
+are reserved for future use. 
+
+FORMAT
+
+00000000-0000-0000-0000-000000000000
+
+Going to call this v1 variant c and Hard code bits 0-12 with with 555
+
+55500000-0000-1000-c000-000000000000
+
+6548C7C1
+
+Bits 13-40 Add the epoch timestamp (as hex) 6548C96E
+
+5556548C-96E0-1000-c000-000000000000
+
+Optional - 1 byte for the ID for the user or system this was generated from 75, 
+placed in bits 45-48 and 53-56 (split because of the version bits) 
+
+5556548C-96E7-1500-c000-000000000000
+32-16-16-16-48
+Last 6 bytes (12 characters) are random.
+That gives you 16 million guids per second per user.
+
+
+Bits 57-64 are reserved for future use.
+Bits 69-80 are user defined bits. 
+
+Bits 81-127
+
